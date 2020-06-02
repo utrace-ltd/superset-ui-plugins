808d7dbab10b84b36ba0123a4b5071afb3696c0c
Lexis <lexis@local>
2020-06-02 23:22:25 +0300

    copied build configs

---
+++ babel.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  "ignore": [
+    "coverage/",
+    "node_modules/",
+    "public/",
+    "esm/",
+    "lib/",
+    "tmp/",
+    "dist/",
+    "*.d.ts",
+    "__tests__",
+    "__mocks__"
+  ],
+  "plugins": [
+    [
+      "babel-plugin-transform-dev",
+      {
+        "evaluate": false
+      }
+    ],
+    "babel-plugin-typescript-to-proptypes",
+    "@babel/plugin-proposal-class-properties"
+  ],
+  "presets": [
+    [
+      "@babel/preset-env",
+      {
+        "loose": true,
+        "modules": false,
+        "shippedProposals": true,
+        "targets": false
+      }
+    ],
+    "@babel/preset-react",
+    "@babel/preset-typescript"
+  ]
+};
